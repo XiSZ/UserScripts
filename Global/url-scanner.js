caef9798c8327d89127b7841e47b503753713aae
XiSZ <XiSZ@users.noreply.github.com>
2025-12-16 19:23:44 +0100

    Update URLScan script metadata and correct updateURL reference

--- Global/url-scanner.js
+++ Global/url-scanner.js
@@ -6,7 +6,7 @@
 // @author       XiSZ
 // @icon         https://avatars.githubusercontent.com/u/40718990
 // @homepage     https://github.com/XiSZ/UserScripts
-// @updateURL    https://cdn.jsdelivr.net/gh/XiSZ/UserScripts@main/Global/url-scanner.js
+// @updateURL    https://cdn.jsdelivr.net/gh/XiSZ/UserScripts@main/Global/url-scanner.meta.js
 // @downloadURL  https://cdn.jsdelivr.net/gh/XiSZ/UserScripts@main/Global/url-scanner.js
 // @supportURL   https://github.com/XiSZ/UserScripts/issues
 // @match        *://*/*
---
+++ Global/url-scanner.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         URLScan by XiSZ
+// @namespace    http://tampermonkey.net/
+// @version      0.03
+// @description  Scan URLs using urlscan.io API and display results with a GUI.
+// @author       XiSZ
+// @icon         https://avatars.githubusercontent.com/u/40718990
+// @homepage     https://github.com/XiSZ/UserScripts
+// @updateURL    https://cdn.jsdelivr.net/gh/XiSZ/UserScripts@main/Global/url-scanner.meta.js
+// @downloadURL  https://cdn.jsdelivr.net/gh/XiSZ/UserScripts@main/Global/url-scanner.js
+// @supportURL   https://github.com/XiSZ/UserScripts/issues
+// @match        *://*/*
+// @grant        GM_xmlhttpRequest
+// @grant        GM_addStyle
+// @connect      urlscan.io
+// ==/UserScript==
